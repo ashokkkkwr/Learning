c9b16ef9d8a284ad46d47e34a19837f472f4b6fd
AshokC7 <ashokkatwal9811@gmail.com>
2024-06-05 17:00:31 +0545

    commited

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const arr=[1,2,3,4,5,6]
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+     {
+      arr.map(item=>(
+        <span >{item}</span>
+      )
+
+      )
+      
+     }
+     {
+      arr.map((num)=><span>{num}</span>)
+     }
         <a
+
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+    </a>
     </div>
   );
 }
