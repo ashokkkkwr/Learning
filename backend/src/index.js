22af32c10fc37f92c1b1d1e785c1a9d99bf99a52
AshokC7 <ashokkatwal9811@gmail.com>
2024-06-11 10:22:05 +0545

    js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,4 +14,20 @@ connectDB()
 })
 console.log(dfhs)
 console.log(SharedArrayBuffer)
+console.log(dsj)import 'dotenv/config'
+
+import connectDB from './db/db.js';
+
+import {app} from './app.js'
+// connect to db
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`connected to the port ${process.env.PORT}`)
+    })
+})
+console.log(dfhs)
+console.log(SharedArrayBuffer)
 console.log(dsj)
