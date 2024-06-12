374ab279583a68a82f624b297d0f0a302800c0a5
AshokC7 <ashokkatwal9811@gmail.com>
2024-06-12 17:53:21 +0545

    hdfs

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+
+import connectDB from './db/db.js';
+
+import {app} from './app.js'
+// connect to db
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`connected to the port ${process.env.PORT}`)
+    })
+})
+
+
+
+
+console.log(fdshf)
